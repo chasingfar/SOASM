88c86a33f721bf203d27de77c1375da329249c31
chasingfar <chasingfar@gmail.com>
2023-05-21 23:41:46 +0800

    add InstrArgs

--- src/soisv1/model.cpp
+++ src/soisv1/model.cpp
@@ -2,26 +2,18 @@
 
 using namespace SOASM::SOISv1;
 
-template<typename T,size_t I>
-using arg_raw=std::tuple_element_t<I,typename T::args>;
-template<typename T,size_t I>
-using arg_raw_t=arg_raw<T,I>::type;
-template<typename T>
-static constexpr size_t arg_raw_num=std::tuple_size_v<typename T::args>;
-
 bool Context::run() {
 	auto pc_old=pc;
 	InstrSet::visit([&]<typename T>(T instr_obj){
-		if constexpr(0==arg_raw_num<T>){
+		if constexpr(0==T::args_t::num){
 			run_instr(instr_obj);
 		}else{
+			auto arg_bytes=mem.get_bytes<T::args_t::size>(pc+1);
+			auto arg_raws=T::args_t::from_bytes(arg_bytes);
+			pc+=T::args_t::size;
 			[&]<size_t ...I>(std::index_sequence<I...>){
-				std::array<std::variant<arg_raw_t<T,I>...>,arg_raw_num<T>> args{
-					arg_raw_t<T,I>{0}...
-				};
-				((args[I]=imm<arg_raw<T,I>>()),...);
-				run_instr(instr_obj,std::get<arg_raw_t<T,I>>(args[I])...);
-			}(std::make_index_sequence<arg_raw_num<T>>{});
+				run_instr(instr_obj,std::get<I>(arg_raws)...);
+			}(std::make_index_sequence<T::args_t::num>{});
 		}
 	},mem.get(pc));
 	return pc_old!=pc;
--- src/types.hpp
+++ src/types.hpp
@@ -120,11 +120,39 @@ namespace SOASM{
 		} // BE
 	} // RawTypes
 
+	template<typename ...Args>
+	struct InstrArgs{
+		static constexpr size_t num = sizeof...(Args);
+		static constexpr size_t size = (0 + ... + Args::size);
+		template<size_t I>
+		using raw=std::tuple_element_t<I,std::tuple<Args...>>;
+		template<typename T,size_t I>
+		using raw_t=raw<I>::type;
+
+		using raws_t=std::tuple<typename Args::type...>;
+
+		static raws_t from_bytes(std::span<uint8_t> data){
+			if constexpr(num==0){
+				return {};
+			}else{
+				return [&]<size_t ...I>(std::index_sequence<I...>)->raws_t{
+					std::array<size_t,num+1> offsets{0};
+					((offsets[I+1]=offsets[I]+raw<I>::size),...);
+					return {Args::from_bytes(data.subspan(offsets[I]))...};
+				}(std::make_index_sequence<num>{});
+			}
+		}
+	};
+
 	template<typename Raw,typename Instr,typename ...Args>
 	struct InstrBase{
 		using raw_t = Raw::type;
-		using args = std::tuple<Args...>;
-		static constexpr size_t size = (sizeof(Raw::size) + ... + Args::size);
+		using args_t = InstrArgs<Args...>;
+		static constexpr size_t size = sizeof(Raw::size) + args_t::size;
+		struct Record{
+			Instr instr;
+			args_t::raws_t args;
+		};
 
 		template<typename InstrSet>
 		void set_id(){
